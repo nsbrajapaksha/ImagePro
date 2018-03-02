23d0a2cf6ac959ed75e488e8409dfd7e85a8e8a7
nsbrajapaksha <nshalitha7@gmail.com>
2018-03-02 15:05:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/imagepro/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.android.imagepro;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+public class MainActivity extends AppCompatActivity {
+
+    private StorageReference mStorageRef;
+    private static final int GALLERY_INTENT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+    }
+
+    public void buttonClicked(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_INTENT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GALLERY_INTENT && requestCode == RESULT_OK) {
+            Uri uri = data.getData();
+            StorageReference filepath = mStorageRef.child("Photos").child(uri.getLastPathSegment());
+            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(MainActivity.this, "Image Uploaded", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
